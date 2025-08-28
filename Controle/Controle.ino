4942345e46432bda3a8fc6092469e96e4b4a5365
Filipe Silva <filipess671@gmail.com>
2025-08-28 20:38:03 -0300

    Ajustando para os 10 botoes

--- Controle/Controle.ino
+++ Controle/Controle.ino
@@ -6,65 +6,92 @@
 USBHIDGamepad gamepad;
 
 // --- Mapeamento dos Pinos ---
-// Eixos do Joystick
+// Joystick 1
 const int PIN_AXIS_X = 1;
 const int PIN_AXIS_Y = 2;
 
-// Botão do clique do Joystick
-const int PIN_BUTTON_SW = 3;
+// Joystick 2
+const int PIN_AXIS_RX = 3;
+const int PIN_AXIS_RY = 4;
+
+// Botões (1 a 8)
+const int PIN_BUTTON_SW_1 = 5;
+const int PIN_BUTTON_SW_2 = 6;
+const int PIN_BUTTON_SW_3 = 7;
+const int PIN_BUTTON_SW_4 = 8;
+const int PIN_BUTTON_SW_5 = 9;
+const int PIN_BUTTON_SW_6 = 10;
+const int PIN_BUTTON_SW_7 = 11;
+const int PIN_BUTTON_SW_8 = 12;
+
+// Botões do segundo joystick
+const int PIN_BUTTON_SW_9  = 13;
+const int PIN_BUTTON_SW_10 = 14;
 
 void setup() {
   Serial.begin(115200);
 
-  // Configura os pinos dos botões como entrada com pull-up interno
-  pinMode(PIN_BUTTON_SW, INPUT_PULLUP);
-  
-  // Inicia o Gamepad
+  // Configura todos os pinos dos botões como entrada com pull-up interno
+  pinMode(PIN_BUTTON_SW_1, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_2, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_3, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_4, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_5, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_6, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_7, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_8, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_9, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_SW_10, INPUT_PULLUP);
+
+  // Inicia o Gamepad e o USB
   gamepad.begin();
-
-  // Inicia o USB
   USB.begin();
-
-  Serial.println("Controle HID iniciado.");
 }
 
 void loop() {
-  // 1. LER OS VALORES ANALÓGICOS E DIGITAIS
-  int xValue = analogRead(PIN_AXIS_X);
-  int yValue = analogRead(PIN_AXIS_Y);
-  bool buttonState = (digitalRead(PIN_BUTTON_SW) == LOW);
+  // Leitura dos eixos dos dois joysticks
+  int xValue  = analogRead(PIN_AXIS_X);
+  int yValue  = analogRead(PIN_AXIS_Y);
+  int rxValue = analogRead(PIN_AXIS_RX);
+  int ryValue = analogRead(PIN_AXIS_RY);
 
-  // 2. PREPARAR OS DADOS PARA A FUNÇÃO send()
+  // Mapeamento para -127 a 127
+  int8_t mappedX  = map(xValue,  0, 4095, -127, 127);
+  int8_t mappedY  = map(yValue,  0, 4095, 127, -127);
+  int8_t mappedRX = map(rxValue, 0, 4095, -127, 127);
+  int8_t mappedRY = map(ryValue, 0, 4095, 127, -127);
 
-  // Mapear os eixos para a faixa int8_t (-127 a 127)
-  int8_t mappedX = map(xValue, 0, 4095, -127, 127);
-  int8_t mappedY = map(yValue, 0, 4095, 127, -127);
+  // Deadzone
+  if (abs(mappedX) < 10)  mappedX  = 0;
+  if (abs(mappedY) < 10)  mappedY  = 0;
+  if (abs(mappedRX) < 10) mappedRX = 0;
+  if (abs(mappedRY) < 10) mappedRY = 0;
 
-  // Pequena "zona morta" (deadzone)
-  if (abs(mappedX) < 10) mappedX = 0;
-  if (abs(mappedY) < 10) mappedY = 0;
-
-  // Criar a máscara de bits para os botões
+  // Máscara de botões
   uint32_t buttonMask = 0;
-  if (buttonState) {
-    buttonMask |= (1 << 0);
-    //buttonMask 2 |= (2 << 0);
-    //buttonMask 3 |= (3 << 0);
-    //buttonMask 4 |= (4 << 0);
-  }
 
-  // 3. ENVIAR TODOS OS DADOS DE UMA SÓ VEZ
+  if (digitalRead(PIN_BUTTON_SW_1)  == LOW) buttonMask |= (1 << 0);
+  if (digitalRead(PIN_BUTTON_SW_2)  == LOW) buttonMask |= (1 << 1);
+  if (digitalRead(PIN_BUTTON_SW_3)  == LOW) buttonMask |= (1 << 2);
+  if (digitalRead(PIN_BUTTON_SW_4)  == LOW) buttonMask |= (1 << 3);
+  if (digitalRead(PIN_BUTTON_SW_5)  == LOW) buttonMask |= (1 << 4);
+  if (digitalRead(PIN_BUTTON_SW_6)  == LOW) buttonMask |= (1 << 5);
+  if (digitalRead(PIN_BUTTON_SW_7)  == LOW) buttonMask |= (1 << 6);
+  if (digitalRead(PIN_BUTTON_SW_8)  == LOW) buttonMask |= (1 << 7);
+  if (digitalRead(PIN_BUTTON_SW_9)  == LOW) buttonMask |= (1 << 8);
+  if (digitalRead(PIN_BUTTON_SW_10) == LOW) buttonMask |= (1 << 9);
+
+  // Enviar para o host
   gamepad.send(
-    mappedX,      // int8_t x
-    mappedY,      // int8_t y
-    0,            // int8_t z
-    0,            // int8_t rz
-    0,            // int8_t rx
-    0,            // int8_t ry
-    0,            // uint8_t hat
-    buttonMask    // uint32_t buttons
+    mappedX,       // X
+    mappedY,       // Y
+    0,             // Z
+    0,             // RZ
+    mappedRX,      // RX
+    mappedRY,      // RY
+    0,             // Hat
+    buttonMask     // Botões
   );
 
-  // Pequeno delay para não sobrecarregar a comunicação USB
-  delay(10); 
+  delay(10);
 }
